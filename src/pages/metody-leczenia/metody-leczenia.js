b7baacb75000d6576978c1ad51fcee8998ca8ce5
MalinowskiDesigns <malinowskidesignsss@gmail.com>
2025-06-16 10:01:43 +0200

    feat: add swipe animation and update styles for treatments section
    - Added a new swipe animation keyframe in _animations.scss.
    - Updated main.scss to include animations.
    - Removed unused _age.scss and _treatments.scss files.
    - Changed background color of consequences section to white.
    - Introduced secondary CTA styles in _cta.scss.
    - Created new treatments section with responsive design in _treatments.scss.
    - Added new treatments-hero component with video integration.
    - Created new metody-leczenia page with relevant styles and scripts.
    - Added new secondary CTA section for scheduling appointments.
    - Updated styles for age section to include swipe functionality.

--- src/pages/metody-leczenia/metody-leczenia.js
+++ src/pages/metody-leczenia/metody-leczenia.js
@@ -1,4 +1,4 @@
-import './contact.scss';
+import './metody-leczenia.scss';
 
 import '@common/svg-icons.js';
 import '@common/nav-handle.js';
--- src/pages/zaburzenia-erekcji/zaburzenia-erekcji.js
+++ src/pages/zaburzenia-erekcji/zaburzenia-erekcji.js
@@ -3,3 +3,4 @@ import './zaburzenia-erekcji.scss';
 import '@common/svg-icons.js';
 import '@common/nav-handle.js';
 import '@js/play-btn.js';
+import '@js/specialists.js';
